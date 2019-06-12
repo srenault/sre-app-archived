a6a0b575889b19dfaaa70f06035c3003de413b2b
Sebastien Renault <srenault.contact@gmail.com>
2019-06-12 09:34:18 +0200

    Add mock mode

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import apiClientMock from './ApiClient/mock';
 document.addEventListener('deviceready', () => {
   const el = document.getElementById('app');
 
-  const apiClient = Config.prod ? new ApiClient({ endpoint: Config.endpoint }) : apiClientMock;
+  const apiClient = Config.mock ? apiClientMock : new ApiClient({ endpoint: Config.endpoint });
 
   ReactDOM.render(<App apiClient={apiClient} />, el);
 }, false);
