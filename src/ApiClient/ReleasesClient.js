5a6da4ce939a7d64ded7a3ce1620ac87382e8d23
Sebastien Renault <srenault.contact@gmail.com>
2020-01-20 19:21:55 +0100

    Add support for Basic Auth

--- src/ApiClient/ReleasesClient.js
+++ src/ApiClient/ReleasesClient.js
@@ -1,11 +1,12 @@
 export default class ReleasesClient {
-  constructor({ endpoint }) {
+  constructor({ endpoint, request }) {
     if (!endpoint) throw new Error('Please specify endpoint');
     this.endpoint = endpoint;
+    this.request = request;
   }
 
   async list() {
-    const response = await fetch(`${this.endpoint}`);
+    const response = await this.request(`${this.endpoint}`);
     return response.json();
   }
 }
